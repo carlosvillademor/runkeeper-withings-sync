663ddedadb8454392b5fb2df03fb625763084d4e
Carlos Fernandez Garcia <carlos.villademor@gmail.com>
2015-03-16 09:27:40 +0000

    Changing port on http server

--- app/runkeeper-api.js
+++ app/runkeeper-api.js
@@ -15,7 +15,7 @@ var options = {
 var runkeeper = require('runkeeper-js'),
     client = new runkeeper.HealthGraph(options);
 
-var server = app.listen(8080, function() {
+var server = app.listen(5000, function() {
   var host = server.address().address
   var port = server.address().port
   console.log('Example app listening at http://%s:%s', host, port)
